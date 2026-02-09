e109d6bff779562c5c841dc3ccfe876fc8d31dd9
Nils Jessen <nils.jessen@posteo.de>
2026-02-09 15:15:34 +0100

    substract added

--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,5 @@
 def add(x,y):
     return x+y
+
+def subtract(a,b):
+    return a-b
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -4,4 +4,4 @@ import pytest
 def test_add():
     assert add(1,2) == 3
     assert add(-2,2) == 0
-    assert add(0,0) == 1 # hahah!
+    assert add(0,0) == 0
